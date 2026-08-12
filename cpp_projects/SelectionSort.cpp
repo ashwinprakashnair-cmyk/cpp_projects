07298c0e784071b8e918ad614e19abcccad43777
ashwinprakashnair-cmyk <ashwinprakashnair-cmyk>
2026-08-12 15:31:37 +0530

    Added file

--- cpp_projects/SelectionSort.cpp
+++ cpp_projects/SelectionSort.cpp
@@ -1,31 +1,37 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-  int A[5],i,min;
-  
-  cout << "\n Enter Array Elements: ";
-  for (i=0; i<5; i++)
-  {
-    cin >> A[i];
-  }
-  
-  min = A[0];
-  
-  cout << "\n Array Elements are : ";
-  for (i=0; i<5; i++)
-  {
-    cout << A[i] << "\t";
-  }
-  
-  for (i=0;i<5;i++)
-  {
-    if (A[i]<min)
+    int A[5], i, j, min, temp;
+
+    cout << "Enter 5 elements: ";
+    for(i = 0; i < 5; i++)
     {
-      min = A[i];
+        cin >> A[i];
     }
-  }
-  cout << "\n Minimun Element is : " << min;
-  
-return 0;
+
+    for(i = 0; i < 4; i++)
+    {
+        min = i;
+        for(j = i + 1; j < 5; j++)
+        {
+            if(A[j] < A[min])
+            {
+                min = j;
+            }
+        }
+        
+        temp = A[i];
+        A[i] = A[min];
+        A[min] = temp;
+    }
+
+    cout << "Sorted array: ";
+    for(i = 0; i < 5; i++)
+    {
+        cout << A[i] << "\t";
+    }
+
+    return 0;
 }
