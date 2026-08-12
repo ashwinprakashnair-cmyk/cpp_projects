e833f0364572fb106159eeaf6320dd9d44395ab0
ashwinprakashnair-cmyk <ashwinprakashnair-cmyk>
2026-08-12 15:03:27 +0530

    Added new files

---
+++ cpp_projects/AreaCircle.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int r;
+  float area, pi = 3.142;
+  
+  cout << "Enter radius: ";
+  cin >> r;
+  
+  area = pi*r*r;
+  
+  cout << "The radius of the circle is : " << area;
+
+  
+return 0;
+}
---
+++ cpp_projects/EvenNumber.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int num;
+  
+  for(num=1;num<=20;num++);
+  {
+    while(num%2==0)
+
+return 0;
+}
