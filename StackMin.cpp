c7ac7fdab1ea0602b5fb454a490dd71272b7afc4
Ashwin P Nair <ashwinprakashnair@gmail.com>
2026-09-10 14:55:54 +0530

    Implement MinStack with push, pop, top, and getMin
    This is to find the min of element of the stack, and it gets updated after checking every time a new element is added

---
+++ StackMin.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+#define MAX 100
+
+using namespace std;
+
+class MinStack {
+private:
+    int data[MAX]; 
+    int topIndex;
+    int minVal;
+
+public:
+    MinStack() {
+        topIndex = -1;
+        minVal = 0;
+    }
+
+    void push(int x) {
+        if (topIndex >= MAX - 1) {
+            return;
+        }
+
+        if (topIndex == -1) {
+            topIndex++;
+            data[topIndex] = x;
+            minVal = x;
+        } else if (x >= minVal) {
+            topIndex++;
+            data[topIndex] = x;
+        } else {
+            topIndex++;
+            data[topIndex] = 2 * x - minVal;
+            minVal = x;
+        }
+    }
+
+    void pop() {
+        if (topIndex < 0) {
+            return;
+        }
+
+        if (data[topIndex] < minVal) {
+            minVal = 2 * minVal - data[topIndex];
+        }
+        topIndex--;
+    }
+
+    int top() {
+        if (topIndex < 0) return -1;
+        
+        if (data[topIndex] < minVal) {
+            return minVal;
+        }
+        return data[topIndex];
+    }
+
+    int getMin() {
+        if (topIndex < 0) return -1;
+        return minVal;
+    }
+};
